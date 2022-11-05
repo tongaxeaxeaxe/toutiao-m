40a74759fb02768d5b73a2d9df6b7e47d8f11b71
axeaxe <1874320064@qq.com>
2022-11-05 19:33:40 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+
+// 注册使用Vant组件库 把Vant添加为Vue的插件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
